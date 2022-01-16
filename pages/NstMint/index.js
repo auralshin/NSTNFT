2191a071b2de423c38861233e626c416ab8dfe9b
auralshin <anshulspartan141@gmail.com>
2022-01-16 18:33:56 +0530

    stagging

---
+++ pages/NstMint/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NstMint() {
+    return (
+        <div>
+            This is true
+        </div>
+    )
+}
+
+export default NstMint
---
+++ python-scripts/app.py
@@ -0,0 +1,28 @@
+from crypt import methods
+from flask import Flask
+import matplotlib.pylab as plt
+import numpy as np
+# import tensorflow as tf
+# import tensorflow_hub as hub
+
+app = Flask(__name__)
+
+
+@app.route("/<content_path>/", methods=['GET'])
+def nstModelWaves(content_path):
+    # content_image = plt.imread(content_path)
+    # style_image = plt.imread("https://upload.wikimedia.org/wikipedia/commons/0/0a/The_Great_Wave_off_Kanagawa.jpg")
+    # content_image = content_image.astype(np.float32)[np.newaxis, ...] / 255.
+    # style_image = style_image.astype(np.float32)[np.newaxis, ...] / 255.
+    # style_image = tf.image.resize(style_image, (256, 256))
+    # # Load image stylization module.
+    # hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+
+    # # Stylize image.
+    # outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
+    # stylized_image = outputs[0]
+    return "{content_path}"
+
+
+if __name__ == '__name__':
+    app.run(debug=True)
